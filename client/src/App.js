e39aa6a475ba9e481c5e73517d6b47cc44cecbe4
Unnat <unnatuni@gmail.com>
2019-04-24 12:10:28 +0800

    Added React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,48 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./utils/getWeb3";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./views/Home.js";
+import Candidates from "./views/Candidates.js";
+import Voting from "./views/Voting.js";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+    componentDidMount = async () => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    };
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+    runExample = async () => {
+        
+    };
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+    render() {
+        return (
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/candidates">Candidate</Link>
+                        </li>
+                        <li>
+                            <Link to="/voting">Voting</Link>
+                        </li>
+                    </ul>
+                    <hr />
+                    <Route exact path="/" component={Home} />
+                    <Route path="/candidates" component={Candidates} />
+                    <Route path="/voting" component={Voting} />
+                </div>
+            </Router>
+        );
     }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
-  }
 }
 
 export default App;
---
+++ client/src/views/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "../App.css";
+
+class Home extends Component {
+    state = { user: null };
+
+    componentDidMount = async () => {
+        try {
+
+        } catch (error) {
+
+        }
+    };
+
+    runExample = async () => {
+        
+    };
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Home;
