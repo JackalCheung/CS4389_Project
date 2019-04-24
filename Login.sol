901fe24f3ecbf444cf4538ddf0f50c2c01010b5e
JackalCheung <44915725+JackalCheung@users.noreply.github.com>
2019-04-24 11:28:43 +0800

    Update Login.sol

--- Login.sol
+++ Login.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.5.2;
 
 contract Login {
+    
+    address[] users;
     struct userInfo {
         uint userID;
         string userName;
@@ -9,10 +11,36 @@ contract Login {
     
     mapping(address=>userInfo) userList;
     
-    event Registered(address,string);
-    event Role(string);
+    event userRegister(address,string);
+    event returnInfo(address,uint,string,string);
+    event userRole(string);
     
-    function login(address user) public {
+    function login(address _user, string memory _name, string memory _role) public {
+        uint i;
+        userInfo memory u;
+        for(i=0;i<users.length;i++) {
+            if(users[i]==_user) {
+                u = userList[_user];
+                emit returnInfo(address(this)
+                ,u.userID
+                ,u.userName
+                ,u.userRole);
+                break;
+            }
+        }
         
+        if(i == users.length) {
+            u = userInfo(i, _name, _role);
+            users.push(_user);
+            userList[_user] = u;
+            emit userRegister(address(this), _name);
+        }
+    }
+    
+    function getUserInfo(address _user) public {
+        emit returnInfo(address(this)
+        ,userList[_user].userID
+        ,userList[_user].userName
+        ,userList[_user].userRole);
     }
 }
